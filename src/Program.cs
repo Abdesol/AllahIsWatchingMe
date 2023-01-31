fc21e2685b3da19fff4230901945f925b9bffe6d
Abdella Solomon <abdesoltak@gmail.com>
2023-01-31 20:40:18 +0300

    Added prevention of multiple instance of the same app from running

--- src/Program.cs
+++ src/Program.cs
@@ -10,8 +10,14 @@ class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        // don't allow multiple instances of the app
+        if (System.Diagnostics.Process.GetProcessesByName(System.IO.Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetEntryAssembly()!.Location)).Length > 1) 
+            return;
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
